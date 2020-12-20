0601ac602eb5f0b4aba4f14b5d67feeef2c137f9
Sameera <62363395+SameeraMurthy@users.noreply.github.com>
2020-12-20 00:59:53 -0500

    Update template-engine.js

--- dist/template-engine.js
+++ dist/template-engine.js
@@ -1,15 +1,15 @@
 const templator = class {
 	constructor(elements, data) {
-  	this.rawElement = elements
-    this.element = document.querySelector(elements).innerHTML
-    this.data = data
-  }
-  replaceTokens() {
-    return this.element.replace(/\#\{([^}]+)\}/g, (str, match) => {
-      return this.data[match]
-    })
+  		this.rawElement = elements
+		this.element = document.querySelector(elements).innerHTML
+    		this.data = data
 	}
-  init() {
-  	document.querySelector(this.rawElement).innerHTML = this.replaceTokens()
-  }
+	replaceTokens() {
+		return this.element.replace(/\#\{([^}]+)\}/g, (str, match) => {
+			return this.data[match]
+		})
+	}
+	init() {
+		document.querySelector(this.rawElement).innerHTML = this.replaceTokens()
+	}
 }
